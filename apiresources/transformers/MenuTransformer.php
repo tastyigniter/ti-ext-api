32519b4dc7445937f55f256165f5e9e8fd4f0956
Ryan Mitchell <ryan@thoughtcollective.com>
2020-05-05 21:05:32 +0100

    Revert "Make ext-api self contained"
    This reverts commit 9f77258d1af0b263229b7e5d651cd2c2d1a3af81.

--- apiresources/transformers/MenuTransformer.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Igniter\Api\ApiResources\Transformers;
-
-class MenuTransformer extends \Illuminate\Http\Resources\Json\Resource
-{
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
-}
