c82063c5d8299d29eb6f846068772484f032e25f
Ryan Mitchell <ryan@thoughtcollective.com>
2020-05-07 09:48:00 +0100

    Add customers, reviews and reservations end points

---
+++ apiresources/Reviews.php
@@ -0,0 +1,27 @@
+<?php namespace Igniter\Api\ApiResources;
+
+use Igniter\Api\Classes\ApiController;
+
+/**
+ * Reviews API Controller
+ */
+class Reviews extends ApiController
+{
+    public $restConfig = [
+        'actions' => [
+            'index' => [
+                'pageLimit' => 20,
+            ],
+            'store' => [],
+            'show' => [],
+            'update' => [],
+            'destroy' => [],
+        ],
+        'relations' => [
+	       'location',
+	       'customer'
+        ],
+        'model' => \Admin\Models\Reservations_model::class,
+        'transformer' => \Igniter\Api\ApiResources\Transformers\ReservationTransformer::class,
+    ];
+}
