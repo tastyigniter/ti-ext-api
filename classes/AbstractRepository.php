b6b40aad91e750dd2c0cab69379190ec40b9ae05
Ryan Mitchell <ryan@thoughtcollective.com>
2021-01-25 18:57:20 +0000

    Hide relations from main attributes

--- classes/AbstractRepository.php
+++ classes/AbstractRepository.php
@@ -180,6 +180,9 @@ class AbstractRepository
             if ($visible = $this->getVisible())
                 $model->setVisible($visible);
 
+            $keysToRemove = array_keys($model->getRelations());
+            $model->makeHidden($keysToRemove);
+
             $model->bindEvent('model.getAttribute', [$this, 'getModelAttribute']);
             $model->bindEvent('model.setAttribute', [$this, 'setModelAttribute']);
         });
