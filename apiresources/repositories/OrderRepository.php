bba9b4d1010343255d02941335f5fda7083f1061
Ryan Mitchell <ryan@thoughtcollective.com>
2021-01-25 21:29:04 +0000

    I missed this :)

--- apiresources/repositories/OrderRepository.php
+++ apiresources/repositories/OrderRepository.php
@@ -8,9 +8,4 @@ use Igniter\Api\Classes\AbstractRepository;
 class OrderRepository extends AbstractRepository
 {
     protected $modelClass = Orders_model::class;
-
-    public function getOrderTotalAttribute($value)
-    {
-        return currency_json($value);
-    }
 }
