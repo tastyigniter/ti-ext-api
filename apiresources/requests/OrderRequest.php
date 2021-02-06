a79ab2aa2f0c160f5fa3a33b2326d401aad3dfcb
Ryan Mitchell <ryan@thoughtcollective.com>
2021-02-06 16:16:38 +0000

    Bring back missing file

---
+++ apiresources/requests/OrderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Igniter\Api\ApiResources\Requests;
+
+use Request;
+use System\Classes\FormRequest;
+
+class OrderRequest extends FormRequest
+{
+    protected $injectRuleParameters = FALSE;
+
+    public function rules()
+    {
+        $namedRules = [
+            ['first_name', 'lang:igniter.cart::default.checkout.label_first_name', 'required|between:1,48'],
+            ['last_name', 'lang:igniter.cart::default.checkout.label_last_name', 'required|between:1,48'],
+            ['email', 'lang:igniter.cart::default.checkout.label_email', 'sometimes|required|email:filter|max:96'],
+            ['telephone', 'lang:igniter.cart::default.checkout.label_telephone', ''],
+            ['comment', 'lang:igniter.cart::default.checkout.label_comment', 'max:500'],
+            ['payment', 'lang:igniter.cart::default.checkout.label_payment_method', 'sometimes|required|alpha_dash'],
+            ['order_type', 'lang:igniter.cart::default.checkout.label_order_type', 'required'],
+        ];
+
+        if (Request::input('order_type', 'collection') == 'delivery') {
+            $namedRules[] = ['address_id', 'lang:igniter.cart::default.checkout.label_address', 'required|integer'];
+            $namedRules[] = ['address.address_1', 'lang:igniter.cart::default.checkout.label_address_1', 'required|min:3|max:128'];
+            $namedRules[] = ['address.address_2', 'lang:igniter.cart::default.checkout.label_address_2', 'sometimes|min:3|max:128'];
+            $namedRules[] = ['address.city', 'lang:igniter.cart::default.checkout.label_city', 'sometimes|min:2|max:128'];
+            $namedRules[] = ['address.state', 'lang:igniter.cart::default.checkout.label_state', 'sometimes|max:128'];
+            $namedRules[] = ['address.postcode', 'lang:igniter.cart::default.checkout.label_postcode', 'string'];
+            $namedRules[] = ['address.country_id', 'lang:igniter.cart::default.checkout.label_country', 'sometimes|required|integer'];
+        }
+
+        return $namedRules;
+    }
+}
