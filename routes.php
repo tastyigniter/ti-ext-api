e4460a798cf61ca6037f1efbdf2af7ed6f8fabc8
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-07-31 13:54:15 +0100

    Remove /csrf-cookie endpoint #49

--- routes.php
+++ routes.php
@@ -3,11 +3,6 @@
 $api = app('api.router');
 
 $api->version('v1', function ($api) {
-    // Define the routes to issue tokens & initialize CSRF protection.
+    // Define the routes to issue tokens.
     $api->post('/token', [\Igniter\Api\Controllers\Tokens::class, 'create']);
-
-    $api->get(
-        '/csrf-cookie',
-        \Laravel\Sanctum\Http\Controllers\CsrfCookieController::class.'@show'
-    )->middleware('web');
 });
