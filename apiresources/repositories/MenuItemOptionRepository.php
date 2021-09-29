daaf6ead7c96d9f447efa3ba2a53ecac3b2b7b56
Justin v Opijnen <j.vanopijnen@octoblue.nl>
2021-09-29 20:24:18 +0200

    add MenuItemOptionRepository
    override create & update because menu_option_values is not passed as an attribute.
    menu_option_values where ignored

---
+++ apiresources/repositories/MenuItemOptionRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Igniter\Api\ApiResources\Repositories;
+
+use Admin\Models\Menu_item_options_model;
+use Igniter\Api\Classes\AbstractRepository;
+
+class MenuItemOptionRepository extends AbstractRepository
+{
+    protected $modelClass = Menu_item_options_model::class;
+
+    public function create($model, array $attributes)
+    {
+        $this->fireSystemEvent('api.repository.beforeCreate', [$model, $attributes]);
+
+        $this->fill($model, $attributes);
+
+        $created = $model->save();
+
+        $this->fireSystemEvent('api.repository.afterCreate', [$model, $created]);
+
+        return $model;
+    }
+
+    public function update($id, array $attributes = [])
+    {
+        $model = is_numeric($id)
+            ? $this->find($id) : $id;
+
+        if (!$model) return $model;
+
+        $this->fireSystemEvent('api.repository.beforeUpdate', [$model, $attributes]);
+        
+        $this->fill($model, $attributes);
+        
+        $updated = $model->save();
+
+        $this->fireSystemEvent('api.repository.afterUpdate', [$model, $updated]);
+
+        return $model;
+    }
+
+    private function fill($model, $attributes){
+        $model->fill($attributes);
+        if (isset($attributes['menu_option_values']))
+            $model->menu_option_values = $attributes['menu_option_values'];
+    }
+}
