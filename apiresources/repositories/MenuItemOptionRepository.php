fd55c6fe82000d16dceeb7460d676384585ce262
Justin v Opijnen <j.vanopijnen@octoblue.nl>
2021-10-05 19:58:24 +0200

    implemented the fillable

--- apiresources/repositories/MenuItemOptionRepository.php
+++ apiresources/repositories/MenuItemOptionRepository.php
@@ -9,33 +9,5 @@ class MenuItemOptionRepository extends AbstractRepository
 {
     protected $modelClass = Menu_item_options_model::class;
 
-    public function create($model, array $attributes)
-    {
-        $this->fill($model, $attributes);
-
-        return $this->create($model, $attributes);
-    }
-
-    public function update($id, array $attributes = [])
-    {
-        $model = is_numeric($id) ? $this->find($id) : $id;
-
-        if (!$model) return $model;
-
-        $this->fireSystemEvent('api.repository.beforeUpdate', [$model, $attributes]);
-
-        $this->fill($model, $attributes);
-
-        $updated = $model->save();
-
-        $this->fireSystemEvent('api.repository.afterUpdate', [$model, $updated]);
-
-        return $model;
-    }
-
-    private function fill($model, $attributes) {
-        $model->fill($attributes);
-        if (isset($attributes['menu_option_values']))
-            $model->menu_option_values = $attributes['menu_option_values'];
-    }
+    protected $fillable = ["menu_option_values"];
 }
