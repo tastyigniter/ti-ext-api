08fbd0f3ef18f029a7f5a55e0c5963e92d7ce2b9
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2021-12-27 15:17:02 +0000

    Use laravel style validation array
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- apiresources/requests/MenuItemOptionRequest.php
+++ apiresources/requests/MenuItemOptionRequest.php
@@ -6,16 +6,29 @@ use System\Classes\FormRequest;
 
 class MenuItemOptionRequest extends FormRequest
 {
+    public function attributes()
+    {
+        return [
+            'menu_id', lang('admin::lang.menus.label_option'),
+            'option_id', lang('admin::lang.menus.label_option_id'),
+            'priority', lang('admin::lang.menus.label_option'),
+            'required', lang('admin::lang.menus.label_option_required'),
+            'min_selected', lang('admin::lang.menus.label_min_selected'),
+            'max_selected', lang('admin::lang.menus.label_max_selected'),
+            'menu_option_values.*', lang('admin::lang.label_option_value_id'),
+        ];
+    }
+
     public function rules()
     {
         return [
-            ['menu_id', 'admin::lang.menus.label_option', 'nullable|integer'],
-            ['option_id', 'admin::lang.menus.label_option_id', 'required|integer'],
-            ['priority', 'admin::lang.menus.label_option', 'integer'],
-            ['required', 'admin::lang.menus.label_option_required', 'boolean'],
-            ['min_selected', 'admin::lang.menus.label_min_selected', 'integer|lte:max_selected'],
-            ['max_selected', 'admin::lang.menus.label_max_selected', 'integer|gte:min_selected'],
-            ['menu_option_values.*', 'lang:admin::lang.label_option_value_id', 'array'],
+            'menu_id' => ['nullable', 'integer'],
+            'option_id' => ['required', 'integer'],
+            'priority' => ['integer'],
+            'required' => ['boolean'],
+            'min_selected' => ['integer', 'lte:max_selected'],
+            'max_selected' => ['integer', 'gte:min_selected'],
+            'menu_option_values.*' => ['array'],
         ];
     }
 }
