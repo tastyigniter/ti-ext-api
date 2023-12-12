e2b1a10ae3816632636735749f803c749dcb5a30
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2023-12-12 20:49:03 +0000

    Fix code style and phpstan errors

--- src/Exceptions/ValidationHttpException.php
+++ src/Exceptions/ValidationHttpException.php
@@ -10,13 +10,12 @@ class ValidationHttpException extends ResourceException
      * Create a new validation HTTP exception instance.
      *
      * @param \Illuminate\Support\MessageBag|array $errors
-     * @param \Throwable $previous
      * @param array $headers
      * @param int $code
      *
      * @return void
      */
-    public function __construct($errors = null, Throwable $previous = null, $headers = [], $code = 0)
+    public function __construct($errors = null, ?Throwable $previous = null, $headers = [], $code = 0)
     {
         parent::__construct('', $errors, $previous, $headers, $code);
     }
