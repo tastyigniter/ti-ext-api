9cdc466024bee9b9aa585a03c57ad4d9545c7d0c
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2024-01-07 14:35:56 +0000

    Minro refactor
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- src/Classes/AbstractRepository.php
+++ src/Classes/AbstractRepository.php
@@ -228,11 +228,11 @@ class AbstractRepository
             $model->bindEvent('model.setAttribute', [$this, 'setModelAttribute']);
 
             foreach ([
-                'beforeCreate', 'afterCreate',
-                'beforeUpdate', 'afterUpdate',
-                'beforeSave', 'afterSave',
-                'beforeDelete', 'afterDelete',
-            ] as $method) {
+                         'beforeCreate', 'afterCreate',
+                         'beforeUpdate', 'afterUpdate',
+                         'beforeSave', 'afterSave',
+                         'beforeDelete', 'afterDelete',
+                     ] as $method) {
                 $model->bindEvent('model.'.$method, function () use ($model, $method) {
                     if (method_exists($this, $method)) {
                         $this->$method($model);
@@ -286,9 +286,9 @@ class AbstractRepository
             }
 
             $isNested = ($attribute == 'pivot' || (
-                $model->hasRelation($attribute) &&
-                in_array($model->getRelationType($attribute), $singularTypes)
-            ));
+                    $model->hasRelation($attribute) &&
+                    in_array($model->getRelationType($attribute), $singularTypes)
+                ));
 
             if ($isNested && is_array($value) && $model->{$attribute}) {
                 $this->setModelAttributes($model->{$attribute}, $value);
@@ -325,6 +325,10 @@ class AbstractRepository
 
     protected function applyCustomerAwareScope($query)
     {
+        if (!is_array(static::$customerAwareConfig)) {
+            return;
+        }
+
         $columnName = $this->getCustomerAwareColumn();
         if ($columnName && $customer = $this->getCustomerAwareUser()) {
             $query->where($columnName, $customer->getKey());
