ec3d2ace50d081dea9d3de669ec84ba27cff78c1
sampoyigi <sampoyigi@users.noreply.github.com>
2024-01-07 15:13:35 +0000

    PHP Linting (Pint) - Fix styling

--- src/Classes/AbstractRepository.php
+++ src/Classes/AbstractRepository.php
@@ -228,11 +228,11 @@ class AbstractRepository
             $model->bindEvent('model.setAttribute', [$this, 'setModelAttribute']);
 
             foreach ([
-                         'beforeCreate', 'afterCreate',
-                         'beforeUpdate', 'afterUpdate',
-                         'beforeSave', 'afterSave',
-                         'beforeDelete', 'afterDelete',
-                     ] as $method) {
+                'beforeCreate', 'afterCreate',
+                'beforeUpdate', 'afterUpdate',
+                'beforeSave', 'afterSave',
+                'beforeDelete', 'afterDelete',
+            ] as $method) {
                 $model->bindEvent('model.'.$method, function () use ($model, $method) {
                     if (method_exists($this, $method)) {
                         $this->$method($model);
@@ -286,9 +286,9 @@ class AbstractRepository
             }
 
             $isNested = ($attribute == 'pivot' || (
-                    $model->hasRelation($attribute) &&
-                    in_array($model->getRelationType($attribute), $singularTypes)
-                ));
+                $model->hasRelation($attribute) &&
+                in_array($model->getRelationType($attribute), $singularTypes)
+            ));
 
             if ($isNested && is_array($value) && $model->{$attribute}) {
                 $this->setModelAttributes($model->{$attribute}, $value);
