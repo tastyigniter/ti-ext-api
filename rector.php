07f0222df09c8bed460bd497364974a3ee028cfb
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-02-21 19:34:17 +0000

    refactor: add CatchExceptionNameMatchingTypeRector to skipped rules in rector config
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector;
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
@@ -12,6 +13,7 @@ return RectorConfig::configure()
         DeclareStrictTypesRector::class,
     ])
     ->withSkip([
+        CatchExceptionNameMatchingTypeRector::class,
         ReturnTypeFromStrictNewArrayRector::class,
     ])
     ->withPreparedSets(
