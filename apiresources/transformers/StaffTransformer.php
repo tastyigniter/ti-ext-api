7c3f96dc83b9068ba5200a95dfd148ac6df37b1f
Antonio Zhu <antoniozhu@outlook.de>
2025-05-12 20:16:43 +0200

    Added UserTransformer

--- apiresources/transformers/StaffTransformer.php
+++ apiresources/transformers/StaffTransformer.php
@@ -3,19 +3,18 @@
 namespace Igniter\Api\ApiResources\Transformers;
 
 use Admin\Models\Staffs_model;
+use Igniter\Api\ApiResources\Transformers\LocationTransformer;
+use Igniter\Api\ApiResources\Transformers\StaffGroupTransformer;
 use League\Fractal\TransformerAbstract;
 
 class StaffTransformer extends TransformerAbstract
 {
     protected $availableIncludes = [
         'groups',
-        'role'
+        'role',
+        'locations',
+        'user',
     ];
-    
-    public function transform(Staffs_model $staff)
-    {
-        return $staff->toArray();
-    }
 
     public function includeGroups(Staffs_model $staff)
     {
@@ -26,4 +25,20 @@ class StaffTransformer extends TransformerAbstract
     {
         return $this->item($staff->role, new StaffRoleTransformer, 'role');
     }
+
+    public function includeLocations(Staffs_model $staff)
+    {
+        return $this->collection($staff->locations, new LocationTransformer, 'locations');
+    }
+
+    public function includeUser(Staffs_model $staff)
+    {
+        return $this->item($staff->user, new UserTransformer, 'user');
+    }
+
+    public function transform(Staffs_model $staff)
+    {
+        return $staff->toArray();
+    }
+
 }
