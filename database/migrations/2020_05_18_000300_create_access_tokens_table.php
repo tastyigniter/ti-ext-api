12b76a2f9e71e5a89fae8a48ffaa0a16c7eb95c8
Obinna Elvis Okechukwu <dreamor47@gmail.com>
2025-06-12 16:38:03 +0100

    rename the index of columns and keys to fix migration name too long issue

--- database/migrations/2020_05_18_000300_create_access_tokens_table.php
+++ database/migrations/2020_05_18_000300_create_access_tokens_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('igniter_api_access_tokens', function(Blueprint $table): void {
             $table->bigIncrements('id');
-            $table->morphs('tokenable');
+            $table->morphs('tokenable', 'igniter_api_access_tokens_tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
+            $table->string('token', 64)->unique('igniter_api_access_tokens_token_unique');
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
