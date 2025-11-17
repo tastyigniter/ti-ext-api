230cf633747ccbd365f50cfeba0612ccbf6f3315
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2025-11-17 23:00:42 +0000

    feat(api): add ShowTokenUser controller and corresponding route

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::middleware('api')
     ->group(function($router) {
         $router->post('/token', \Igniter\Api\Http\Controllers\CreateToken::class);
     });
+
+Route::middleware(config('igniter-api.middleware'))
+    ->as('igniter.api.token.user')
+    ->prefix(config('igniter-api.prefix'))
+    ->group(function($router) {
+        $router->get('/token/user', \Igniter\Api\Http\Controllers\ShowTokenUser::class);
+    });
